9ef722e1f23a7ee1fe950ddacac5d36b10c8d2cb
nolanspecter <59214460+nolanspecter@users.noreply.github.com>
2020-12-11 18:55:14 +0700

    Finished labwork 5

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -60,7 +60,6 @@ public class ForecastFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
        View view = inflater.inflate(R.layout.fragment_forecast, container, false);
-       view.setBackgroundColor(0x20FF0000);
        return view;
     }
 }
